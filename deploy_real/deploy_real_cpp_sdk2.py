49e9bc9ed9fa76aad1835b04050f7e37f8923379
NanHaibei <371743175@qq.com>
2025-09-23 22:11:57 +0800

    cpp_sdk2替换框架验证通过，BeyondMimic in real 仍然异常抖动

--- deploy_real/deploy_real_cpp_sdk2.py
+++ deploy_real/deploy_real_cpp_sdk2.py
@@ -11,16 +11,6 @@ import time
 import os
 import yaml
 
-from unitree_sdk2py.core.channel import ChannelPublisher, ChannelFactoryInitialize
-from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
-from unitree_sdk2py.idl.default import unitree_hg_msg_dds__LowCmd_, unitree_hg_msg_dds__LowState_
-from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowCmd_, unitree_go_msg_dds__LowState_
-from unitree_sdk2py.idl.unitree_hg.msg.dds_ import LowCmd_ as LowCmdHG
-from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowCmd_ as LowCmdGo
-from unitree_sdk2py.idl.unitree_hg.msg.dds_ import LowState_ as LowStateHG
-from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowState_ as LowStateGo
-from unitree_sdk2py.utils.crc import CRC
-
 # TODO: 更新README，加上unitree_cpp安装方法
 from config_cpp_sdk2 import RobotConfig
 from unitree_cpp import UnitreeController, RobotState, SportState
@@ -73,29 +63,6 @@ class Controller:
         
         self.control_dt = cfg.unitree.control_dt
         
-        
-        
-        
-        
-        # self.config = config
-        # self.num_joints = config.num_joints
-        
-        
-        # self.low_cmd = unitree_hg_msg_dds__LowCmd_()
-        # self.low_state = unitree_hg_msg_dds__LowState_()
-        # self.mode_pr_ = MotorMode.PR
-        # self.mode_machine_ = 0
-        # self.lowcmd_publisher_ = ChannelPublisher(config.lowcmd_topic, LowCmdHG)
-        # self.lowcmd_publisher_.Init()
-        
-        # # inital connection
-        # self.lowstate_subscriber = ChannelSubscriber(config.lowstate_topic, LowStateHG)
-        # self.lowstate_subscriber.Init(self.LowStateHgHandler, 10)
-        
-        # self.wait_for_low_state()
-        
-        # self.init_cmd_hg_cpp(self.low_cmd, self.mode_machine_, self.mode_pr_)
-        
         self.policy_output_action = np.zeros(self.num_dofs, dtype=np.float32)
         self.kps = np.zeros(self.num_dofs, dtype=np.float32)
         self.kds = np.zeros(self.num_dofs, dtype=np.float32)
@@ -105,9 +72,6 @@ class Controller:
         self.ang_vel = np.zeros(3, dtype=np.float32)
         self.gravity_orientation = np.array([0,0,-1], dtype=np.float32)
         
-        
-        
-        # self.counter_over_time = 0
         
     def self_check(self):
         for _ in range(30):
@@ -119,18 +83,6 @@ class Controller:
             print("UnitreeCppEnv self check failed!")
             return False
         return True
-    
-    # def init_cmd_hg_cpp(self, cmd: LowCmdHG, mode_machine: int, mode_pr: int):
-    #     cmd.mode_machine = mode_machine
-    #     cmd.mode_pr = mode_pr
-    #     size = len(cmd.motor_cmd)
-    #     for i in range(size):
-    #         cmd.motor_cmd[i].mode = 1
-    #         cmd.motor_cmd[i].q = 0
-    #         cmd.motor_cmd[i].qd = 0
-    #         cmd.motor_cmd[i].kp = 0
-    #         cmd.motor_cmd[i].kd = 0
-    #         cmd.motor_cmd[i].tau = 0
     
     def update(self):
         self.robot_state = self.unitree.get_robot_state()
@@ -146,14 +98,14 @@ class Controller:
                 [self.robot_state.motor_state.tau_est[self.dof_idx[i]] for i in range(len(self.dof_idx))]
             )
 
-            quat = np.asarray(self.robot_state.imu_state.quaternion)
-            ang_vel = np.array(self.robot_state.imu_state.gyroscope, dtype=np.float32)
+            # quat = np.asarray(self.robot_state.imu_state.quaternion)
+            # ang_vel = np.array(self.robot_state.imu_state.gyroscope, dtype=np.float32)
 
-            self._imu_quaternion = quat[[1, 2, 3, 0]]
-            self._imu_angular_velocity = ang_vel
-            self._imu_angles = (
-                self.robot_state.imu_state.rpy
-            )
+            # self._imu_quaternion = quat[[1, 2, 3, 0]]
+            # self._imu_angular_velocity = ang_vel
+            # self._imu_angles = (
+            #     self.robot_state.imu_state.rpy
+            # )
 
         elif self.msg_type == "go":
             raise NotImplementedError("msg_type 'go' not implemented in this example.")
@@ -177,33 +129,6 @@ class Controller:
         self.remote_controller.set(self.robot_state.wireless_remote)
         # All low_state is equal to robot_state. The repo makes that
         
-        
-        
-        
-        
-        
-    # def LowStateHgHandler(self, msg: LowStateHG):
-    #     self.low_state = msg
-    #     self.mode_machine_ = self.low_state.mode_machine
-    #     self.remote_controller.set(self.low_state.wireless_remote)
-
-    def send_cmd(self, cmd: Union[LowCmdGo, LowCmdHG]):
-        cmd.crc = CRC().Crc(cmd)
-        self.lowcmd_publisher_.Write(cmd)
-
-    # def wait_for_low_state(self):
-    #     while self.low_state.tick == 0:
-    #         time.sleep(self.config.control_dt)
-    #     print("Successfully connected to the robot.")
-
-    def zero_torque_state(self):
-        print("Enter zero torque state.")
-        print("Waiting for the start signal...")
-        while self.remote_controller.button[KeyMap.start] != 1:
-            create_zero_cmd(self.low_cmd)
-            self.send_cmd(self.low_cmd)
-            time.sleep(self.cfg.control_dt)
-        
     def run(self):
         try:
             # if(self.counter_over_time >= config.error_over_time):
@@ -219,13 +144,12 @@ class Controller:
                 self.state_cmd.skill_cmd = FSMCommand.LOCO
             if self.remote_controller.is_button_pressed(KeyMap.X) and self.remote_controller.is_button_pressed(KeyMap.R1):
                 self.state_cmd.skill_cmd = FSMCommand.SKILL_1
-            if self.remote_controller.is_button_pressed(KeyMap.Y) and self.remote_controller.is_button_pressed(KeyMap.R1):
-                self.state_cmd.skill_cmd = FSMCommand.SKILL_2
+            # if self.remote_controller.is_button_pressed(KeyMap.Y) and self.remote_controller.is_button_pressed(KeyMap.R1):
+            #     self.state_cmd.skill_cmd = FSMCommand.SKILL_2
             # if self.remote_controller.is_button_pressed(KeyMap.B) and self.remote_controller.is_button_pressed(KeyMap.R1):
             #     self.state_cmd.skill_cmd = FSMCommand.SKILL_3
             if self.remote_controller.is_button_pressed(KeyMap.Y) and self.remote_controller.is_button_pressed(KeyMap.L1):
                 self.state_cmd.skill_cmd = FSMCommand.SKILL_4  # Beyond Mimic
-            
             
             self.update()
             self.update_remote_controller()
@@ -234,14 +158,16 @@ class Controller:
             self.state_cmd.vel_cmd[2] =  self.remote_controller.rx * -1
 
             for i in range(self.num_dofs):
+                self.qj[i] = self._joint_positions[i]
+                self.dqj[i] = self._joint_velocities[i]
                 # self.qj[i] = self.robot_state.motor_state[i].q  # 什么定义了要q迭代而不是motor_state迭代？
                 # self.dqj[i] = self.robot_state.motor_state[i].dq  # cpp代码中明明是low_state.motor_state()[i].q() 
-                self.qj[i] = self.robot_state.motor_state.q[i]
-                self.dqj[i] = self.robot_state.motor_state.dq[i]
+                # self.qj[i] = self.robot_state.motor_state.q[i]
+                # self.dqj[i] = self.robot_state.motor_state.dq[i]
 
             # imu_state quaternion: w, x, y, z
-            quat = self.robot_state.imu_state.quaternion
-            ang_vel = np.array([self.robot_state.imu_state.gyroscope], dtype=np.float32).squeeze()
+            quat = np.asarray(self.robot_state.imu_state.quaternion)
+            ang_vel = np.array(self.robot_state.imu_state.gyroscope, dtype=np.float32).squeeze()
             
             gravity_orientation = get_gravity_orientation_real(quat)
             
@@ -251,24 +177,13 @@ class Controller:
             self.state_cmd.ang_vel = ang_vel.copy()
             self.state_cmd.base_quat = quat
             
-            self.FSM_controller.run()
+            self.FSM_controller.run()  # policy
             policy_output_action = self.policy_output.actions.copy()
             kps = self.policy_output.kps.copy()
             kds = self.policy_output.kds.copy()
-            
-            # Build low cmd
-            # for i in range(self.num_joints):
-            #     self.low_cmd.motor_cmd[i].q = policy_output_action[i]
-            #     self.low_cmd.motor_cmd[i].qd = 0
-            #     self.low_cmd.motor_cmd[i].kp = kps[i]
-            #     self.low_cmd.motor_cmd[i].kd = kds[i]
-            #     self.low_cmd.motor_cmd[i].tau = 0
-                
-            # # send the command
-            # # create_damping_cmd(controller.low_cmd) # only for debug
-            # self.send_cmd(self.low_cmd)
-            self.set_gains(kps, kds)
-            self.step(policy_output_action)
+
+            self.set_gains(kps, kds)  # set kp, kd
+            self.step(policy_output_action)  # send pd target
             
             loop_end_time = time.time()
             delta_time = loop_end_time - loop_start_time
@@ -287,9 +202,6 @@ class Controller:
         
         
 if __name__ == "__main__":
-    # config = Config()
-    # Initialize DDS communication
-    # ChannelFactoryInitialize(0, config.net)
     cfg = RobotConfig()
     
     controller = Controller(cfg)
@@ -305,7 +217,5 @@ if __name__ == "__main__":
             break
     
     controller.shutdown()
-    # create_damping_cmd(controller.low_cmd)
-    # controller.send_cmd(controller.low_cmd)
     print("Exit")
     
